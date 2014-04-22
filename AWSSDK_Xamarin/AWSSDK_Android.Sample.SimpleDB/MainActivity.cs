b1e48db9b5f58d38a0c47090ea672a220675fd35
TIHan <lol.tihan@gmail.com>
2014-04-22 12:20:01 -0500

    merged

---
+++ AWSSDK_Xamarin/AWSSDK_Android.Sample.SimpleDB/MainActivity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Amazon.SimpleDB;
+
+// http://aws.amazon.com/articles/SDKs/iOS/8829919029640036
+namespace AWSSDK_Android.Sample.SimpleDB
+{
+	[Activity (Label = "AWSSDK_Android.Sample.SimpleDB", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		const string ACCESS_KEY = "";
+		const string SECRET_KEY = "";
+
+		AmazonSimpleDBClient _simpleDBClient = new AmazonSimpleDBClient (ACCESS_KEY, SECRET_KEY, Amazon.RegionEndpoint.USWest1);
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			
+			button.Click += delegate
+			{
+				try
+				{
+					var request = new Amazon.SimpleDB.Model.CreateDomainRequest ();
+					request.DomainName = "HIGH_SCORE_DOMAIN";
+
+					_simpleDBClient.CreateDomain (request);
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine ("CreateDomain: " + e.Message);
+				}
+			};
+				
+		}
+	}
+}
+
+
---
+++ AWSSDK_Xamarin/AWSSDK_iOS.Sample.SimpleDB/AWSSDK_iOS_Sample_SimpleDBViewController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+using Amazon.SimpleDB;
+
+// http://aws.amazon.com/articles/SDKs/iOS/8829919029640036
+namespace AWSSDK_iOS.Sample.SimpleDB
+{
+	public partial class AWSSDK_iOS_Sample_SimpleDBViewController : UIViewController
+	{
+		const string ACCESS_KEY = "";
+		const string SECRET_KEY = "";
+
+		AmazonSimpleDBClient _simpleDBClient = new AmazonSimpleDBClient (ACCESS_KEY, SECRET_KEY, Amazon.RegionEndpoint.USWest1);
+		public AWSSDK_iOS_Sample_SimpleDBViewController () : base ("AWSSDK_iOS_Sample_SimpleDBViewController", null)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			try
+			{
+				var request = new Amazon.SimpleDB.Model.CreateDomainRequest ();
+				request.DomainName = "HIGH_SCORE_DOMAIN";
+
+				_simpleDBClient.CreateDomain (request);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine ("CreateDomain: " + e.Message);
+			}
+		}
+	}
+}
+
