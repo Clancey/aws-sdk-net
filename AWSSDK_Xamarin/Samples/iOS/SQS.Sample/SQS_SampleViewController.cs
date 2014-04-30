0015d73d2ae726ba8d57733b9beea84d443988ab
Rachel Reese <rachel.reese@fireflylogic.com>
2014-04-30 16:10:26 -0500

    iOS SQS example, Android project.

---
+++ AWSSDK_Xamarin/Samples/iOS/SQS.Sample/SQS_SampleViewController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using Amazon.SQS;
+
+namespace SQS.Sample
+{
+	public partial class SQS_SampleViewController : UIViewController
+	{
+		const string ACCESS_KEY = "";
+		const string SECRET_KEY = "";
+
+		public SQS_SampleViewController () : base ("SQS_SampleViewController", null)
+		{
+		}
+			
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			var sqsClient = new Amazon.SQS.AmazonSQSClient (ACCESS_KEY, SECRET_KEY, Amazon.RegionEndpoint.EUWest1);
+			var request = new Amazon.SQS.Model.CreateQueueRequest ();
+			request.QueueName = "newQueue";
+			var response = sqsClient.CreateQueue (request);
+		}
+	}
+}
+
