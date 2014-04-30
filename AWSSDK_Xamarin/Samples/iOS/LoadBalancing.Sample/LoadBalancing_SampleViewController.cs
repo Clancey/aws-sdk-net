01c55c0b0d81d9df35698009c1cbb695f42ff99f
Rachel Reese <rachel.reese@fireflylogic.com>
2014-04-30 11:57:40 -0500

    iOS Load Balancing sample working.

--- AWSSDK_Xamarin/Samples/iOS/LoadBalancing.Sample/LoadBalancing_SampleViewController.cs
+++ AWSSDK_Xamarin/Samples/iOS/LoadBalancing.Sample/LoadBalancing_SampleViewController.cs
@@ -2,28 +2,35 @@
 using System.Drawing;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+using Amazon.ElasticLoadBalancing;
 
 namespace LoadBalancing.Sample
 {
 	public partial class LoadBalancing_SampleViewController : UIViewController
 	{
+		const string ACCESS_KEY = "";
+		const string SECRET_KEY = "";
+		AmazonElasticLoadBalancingClient sLBClient = new AmazonElasticLoadBalancingClient(ACCESS_KEY, SECRET_KEY, Amazon.RegionEndpoint.USEast1);
+
 		public LoadBalancing_SampleViewController () : base ("LoadBalancing_SampleViewController", null)
 		{
-		}
-
-		public override void DidReceiveMemoryWarning ()
-		{
-			// Releases the view if it doesn't have a superview.
-			base.DidReceiveMemoryWarning ();
-			
-			// Release any cached data, images, etc that aren't in use.
 		}
 
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
-			
-			// Perform any additional setup after loading the view, typically from a nib.
+			CreateLoadBalancer.TouchUpInside += HandleTouchUpInside;
+		}
+
+		void HandleTouchUpInside (object sender, EventArgs e)
+		{
+			var request = new Amazon.ElasticLoadBalancing.Model.CreateLoadBalancerRequest ("newLoadBalancer");
+			request.AvailabilityZones = new System.Collections.Generic.List<string>{ "us-east-1b", "us-east-1c" };
+			var listener = new Amazon.ElasticLoadBalancing.Model.Listener ("HTTP", 80, 45);
+			request.Listeners = new System.Collections.Generic.List<Amazon.ElasticLoadBalancing.Model.Listener>{ listener };
+
+			var response = sLBClient.CreateLoadBalancer (request);
+			LoadBalancerStatus.Text = String.Format ("Load balancer {0} was created.", response.DNSName);
 		}
 	}
 }
